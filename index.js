712852e6ba82dd8ae0c8d66c3494dcb36c19dc2c
Khan Meraj <khanmeraj1542005@gmail.com>
2023-05-16 22:04:08 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// let navbar_nav =document.querySelector(".navbar-expand-lg");
+
+
+// window.addEventListener('scroll', function(event) {
+//     event.preventDefault();
+
+//     if (window.scrollY < 200){
+//         navbar_nav.classList.remove("hidden");
+//     }
+// });
