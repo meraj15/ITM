25edae0e92e9b310f4bf38d1575482dae2e70587
Khan Meraj <khanmeraj1542005@gmail.com>
2023-05-19 16:34:36 +0530

    finally done

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
-let animate = document.querySelector(".item-10")
-
-window.addEventListener('scroll', function(event) {
-    event.preventDefault();
-
-    if (window.scrollY < 5600){
-        animate.classList.remove("hidden");
-    }
-    // else{
-    //     animate.classList.add("hidden");
-    // }
-});
-
 $(document).ready(function(){
     $(window).scroll(function(){
         var scroll = $(window).scrollTop();
         if (scroll > 700) {
             $(".navbar").css("background" , "black");
         }
+  
         else{
             $(".navbar").css("background" , "none");  	
         }
     })
   })
+
+//   document.querySelector(".item-first").addEventListener("mouseover",function(){
+//     let vara= document.querySelector(".item-first");
+//     if(mouseover === vara){
+//  vara.style.height = "30rem";
+//     }else{
+//  vara.style.height = "22rem";
+//     }
+//   })
+// let display = document.querySelector(".item-3-container-hover");
+//   document.querySelector(".item-3-btn").addEventListener("click",function(){
+// display.classList.remove("hidden");
+//   })
+
+//   document.querySelector(".item-3-btn").addEventListener("dblclick",function(){
+//     display.classList.add("hidden");
+//       })
+
+
+  
+
+
